3d76be2aa27f2c2d1e327235022473ead3b00db9
Jayasyam <121284762+Jayasyam-K@users.noreply.github.com>
2026-03-25 23:24:11 +0530

    Add files via upload

---
+++ Day 55 : Equal Sum Grid Partition I/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        long totalSum = 0;
+        for (int[] row : grid) {
+            for (int val : row) {
+                totalSum += val;
+            }
+        }
+
+        if (totalSum % 2 != 0) return false;
+        long target = totalSum / 2;
+
+        long currentSum = 0;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                currentSum += grid[i][j];
+            }
+            if (currentSum == target) return true;
+        }
+
+        currentSum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++) {
+                currentSum += grid[i][j];
+            }
+            if (currentSum == target) return true;
+        }
+
+        return false;
+    }
+}
